57d0b651753786ceebb155d83dcd2a5836c3aa94
Sam Lim <limsammy1@gmail.com>
2017-05-02 14:25:22 -0600

    Can add new robots to database

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,8 +3,23 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    redirect '/robots'
+  end
+
   get '/robots' do
+    @robots = Robot.all
     erb :index
+  end
+
+  get '/robots/new' do
+    erb :new
+  end
+
+  post '/robots' do
+    robot = Robot.new(params[:robot])
+    robot.save
+    redirect '/robots'
   end
 end
 
